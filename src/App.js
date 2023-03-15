c23c1d64dc6cca829d8362cdae7743d42fa2645d
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-03-14 22:11:36 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ import 'react-tabs/style/react-tabs.css';
 export default function App() {
   const [dataAPI, setData] = useState([]);
   const [year, setYear] = useState([]);
+
   const url =
     'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
-  const pokeApi = 'https://pokeapi.co/api/v2/pokemon?limit=15';
-
   const fetchAPI = async () => {
     try {
       const resp = await axios
         .get(url)
+        .then((response) => response.data.data)
+        .then((data) => {
+          setData(data);
+        });
+    } catch (error) {}
+  };
+
+  const [pokeDataAPI, pokeSetData] = useState([]);
+  const [pokeyear, setYear] = useState([]);
+
+  const pokeApi = 'https://pokeapi.co/api/v2/pokemon?limit=15';
+  const fetchPokeAPI = async () => {
+    try {
+      const resp = await axios
+        .get(pokeApi)
         .then((response) => response.data.data)
         .then((data) => {
           setData(data);

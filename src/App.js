415cc50044f7a25523ee1157eb40b617e55deac2
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-03-16 22:37:09 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PopulationApi from './components/populationApi';
 import PokeApi from './components/pokeApi';
+import DogApi from './components/dogApi';
 
 export default function App() {
   return (
     <>
       <PopulationApi />
       <PokeApi />
+      <DogApi />
     </>
   );
 }
--- src/components/dogApi.jsx
+++ src/components/dogApi.jsx
@@ -1,4 +1,29 @@
 import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style.css';
 
-export default function dogApi() {}
+export default function dogApi() {
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then((response) => response.json())
+      .then((data) => {
+        // Get the list of breeds from the data object
+        const breeds = Object.keys(data.message);
+        setBreeds(breeds);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="population">
+      <h1>Dog Breeds List</h1>
+      <ul>
+        {breeds.map((breed) => (
+          <li key={breed}>{breed}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

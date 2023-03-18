507d5e3f20c06b55f42c1722a042a76b6c1acb8a
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-03-17 23:05:40 -0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 import PopulationApi from './components/populationApi';
 import PokeApi from './components/pokeApi';
 import DogApi from './components/dogApi';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import DashboardPage from './pages/DashboardPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return (
-    <>
-      <PopulationApi />
-      <PokeApi />
-      <DogApi />
-    </>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/users/*" element={<UserPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/myusers/" element={<Navigate replace to="/users" />} />
+        <Route path="/users/:id" element={<UserPage />} />
+        <Route path="/dashboard/*" element={<DashboardPage />}>
+          <Route path="welcome" element={<p>Welcome!</p>} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
+

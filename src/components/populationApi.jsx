3b6637f75e87912cdc648b3b03b82c02751239c3
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-04-03 23:30:03 -0700

    1 file modified

--- src/components/populationApi.jsx
+++ src/components/populationApi.jsx
@@ -16,7 +16,6 @@ export default function populationApi() {
     try {
       const resp = await axios     
         .get(url)
-
         .then((response) => response.data.data)
         
         .then((data) => {          

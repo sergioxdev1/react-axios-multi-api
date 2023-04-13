a3603ddadd67531a1691bfa3fe148fa54ec1f5a3
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-04-12 22:53:20 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ export default function App() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
+    {window.location.hash="no-back-button"}
+    {window.location.hash="Again-No-back-button"}
+    {window.onhashchange=function(){window.location.hash="no-back-button"}}
     </div>
   );
 }

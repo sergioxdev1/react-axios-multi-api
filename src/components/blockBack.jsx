a8ab9781416af821d3c7bfed3cb371c64c82fdb8
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-04-14 21:12:21 -0700

    1 file modified

--- src/components/blockBack.jsx
+++ src/components/blockBack.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import React, { useEffect, useState } from 'react';
 
-export default function dogApi() {
+export default function BlockBack() {
   {
     window.location.hash = 'no-back-button';
   }

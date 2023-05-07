98f2473f7f002753cb649b9122913627f06f5954
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-05-06 19:35:08 -0700

    1 file modified

--- src/components/dogApi.jsx
+++ src/components/dogApi.jsx
@@ -10,9 +10,10 @@ export default function dogApi() {
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then((response) => response.json())
-      .then((data) => {  
-        // Get the list of breeds from the data object
+      .then((data) => {
         
+        // Get the list of breeds from the data object
+
         const breeds = Object.keys(data.message);
         setBreeds(breeds);
 

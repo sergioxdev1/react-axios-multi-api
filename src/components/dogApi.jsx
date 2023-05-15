796872fc7d6a8e3a35dffa3e6e1875428d51bcdf
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-05-14 22:48:38 -0700

    1 file modified

--- src/components/dogApi.jsx
+++ src/components/dogApi.jsx
@@ -15,7 +15,7 @@ export default function dogApi() {
         const breeds = Object.keys(data.message);
         setBreeds(breeds);
       })
-      
+
       .catch((error) => console.error(error));
   }, []);
 
